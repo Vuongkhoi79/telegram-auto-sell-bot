a57aaeac305ce1ffed34fc47e89340855900dec8
Vuongkhoi79 <87454414+Vuongkhoi79@users.noreply.github.com>
2026-05-21 16:55:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const express = require("express");
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Telegram Auto Sell Bot Running");
+  res.send("Telegram Bot Running");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
+});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    `🔥 Chào mừng tới AI Store
+
+📦 Sản phẩm:
+- Prompt viral Reel
+- Tool AI
+- ChatGPT Plus
+- Claude
+- Gemini
+
+💰 Liên hệ admin để mua`
+  );
 });
